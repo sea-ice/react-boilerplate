2f9fd6d4e2dd7b7b1d53d45056cb9b789ecc8308
Jack <seaice.chou@gmail.com>
2018-07-28 12:02:33 +0800

    chore: delete uncalled code

--- server/plugins/ssr/index.js
+++ server/plugins/ssr/index.js
@@ -16,30 +16,6 @@ module.exports = {
       docTitle: 'Document'
     }, pluginConfig)
 
-    // 增加render方法
-    // app.context.render = (view, locals, opts, children) => {
-    //   opts = Object.assign(options, opts)
-    //   let render = opts.internals ? ReactDOMServer.renderToString : ReactDOMServer.renderToStaticMarkup
-    //   let viewPath = path.resolve(opts.viewPath, `${view}${opts.extName}`)
-    //   let markup = opts.docType
-
-    //   let Container = require(viewPath)
-    //   Container = Container.default || Container
-
-    //   locals.metaData = Object.assign({
-    //     docTitle: 'Document',
-    //     scripts: [],
-    //     styles: []
-    //   }, locals.metaData)
-    //   let metaData = locals.metaData
-    //   metaData.scripts = metaData.scripts.map(src => src.match(/^https?:\/\//) ? src : `${opts.serverRoot}${src}`)
-    //   metaData.styles = metaData.styles.map(style => style.match(/^https?:\/\//) ? style : `${opts.serverRoot}${style}`)
-
-    //   markup += render(React.createElement(Container, locals, children))
-    //   // console.log(markup)
-    //   return markup
-    // }
-
     // locals: {
     //   initialState: {},
     //   metaData: {
